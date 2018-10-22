8c42fa41ba0ee7ca8a546798d265e993532551f4
adamxtokyo <adam.grawender@gmail.com>
2018-10-22 19:09:51 +0200

    Refactored code where needed

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-var through = require('through2');
-var Vinyl = require('vinyl');
-var JavaScriptObfuscator = require('javascript-obfuscator');
-var PluginError = require('plugin-error');
+const through2 = require('through2');
+const Vinyl = require('vinyl');
+const JavaScriptObfuscator = require('javascript-obfuscator');
+const PluginError = require('plugin-error');
 
-module.exports = function gulpJavaScriptObfuscator(options) {
-	if(!options) {
-		options = {}
-	}
+module.exports = function gulpJavaScriptObfuscator (options = {}) {
+	return through2.obj(function (file, enc, cb) {
+		if (file.isNull()) return cb(null, file);
+		if (!file.isBuffer()) throw new PluginError('gulp-javascript-obfuscator', 'Only Buffers are supported!');
 
-	return through.obj(function (file, enc, cb) {
-		var obfuscationResult;
-		if (file.isNull()) {
+		try {
+			const obfuscationResult = JavaScriptObfuscator.obfuscate(String(file.contents), options);
+			file.contents = new Buffer(obfuscationResult.getObfuscatedCode());
+			if (options.sourceMap && options.sourceMapMode !== 'inline') {
+				this.push(new Vinyl({
+					cwd: file.cwd,
+					base: file.base,
+					path: file.path + '.map',
+					contents: new Buffer(obfuscationResult.getSourceMap())
+				}))
+			}
 			return cb(null, file);
-		}
-
-		if (file.isBuffer()) {
-			try {
-				obfuscationResult = JavaScriptObfuscator.obfuscate(String(file.contents), options);
-				file.contents = new Buffer(obfuscationResult.getObfuscatedCode());
-				if(options.sourceMap && options.sourceMapMode !== 'inline') {
-					this.push(new Vinyl({
-						cwd: file.cwd,
-						base: file.base,
-						path: file.path + '.map',
-						contents: new Buffer(obfuscationResult.getSourceMap())
-					}))
-				}
-				cb(null, file);
-			}
-			catch (err) {
-				throw new PluginError('gulp-javascript-obfuscator', err);
-			}
-		} else if (file.isStream()) {
-			throw new PluginError('gulp-javascript-obfuscator', 'Streams are not supported!');
+		} catch (err) {
+			throw new PluginError('gulp-javascript-obfuscator', err);
 		}
 	});
 };
