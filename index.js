4c1acb4e9d8771d230cd97d46b0a3059ceef8d7d
David Refoua <David@Refoua.me>
2019-04-19 19:02:40 +0430

    Use `gulp-sourcemaps` for source maps (#18)
    * use gulp-sourcemaps instead
    
    * update readme

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ const through2 = require('through2');
 const Vinyl = require('vinyl');
 const JavaScriptObfuscator = require('javascript-obfuscator');
 const PluginError = require('plugin-error');
+const applySourceMap = require('vinyl-sourcemaps-apply');
 
 module.exports = function gulpJavaScriptObfuscator (options = {}) {
 	return through2.obj(function (file, enc, cb) {
 		if (file.isNull()) return cb(null, file);
 		if (!file.isBuffer()) throw new PluginError('gulp-javascript-obfuscator', 'Only Buffers are supported!');
+		if (file.sourceMap) {
+			options.sourceMap = true;
+			options.inputFileName = file.relative;
+			options.sourceMapMode = 'separate';
+		}
 
 		try {
 			const obfuscationResult = JavaScriptObfuscator.obfuscate(String(file.contents), options);
 			file.contents = new Buffer(obfuscationResult.getObfuscatedCode());
 			if (options.sourceMap && options.sourceMapMode !== 'inline') {
-				this.push(new Vinyl({
-					cwd: file.cwd,
-					base: file.base,
-					path: file.path + '.map',
-					contents: new Buffer(obfuscationResult.getSourceMap())
-				}))
+				if ( file.sourceMap ) {
+					const sourceMap = JSON.parse(obfuscationResult.getSourceMap());
+					sourceMap.file = file.sourceMap.file;
+					applySourceMap(file, sourceMap);
+				}
+				else {
+					this.push(new Vinyl({
+						cwd: file.cwd,
+						base: file.base,
+						path: file.path + '.map',
+						contents: new Buffer(obfuscationResult.getSourceMap())
+					}))
+				}
 			}
 			return cb(null, file);
 		} catch (err) {
